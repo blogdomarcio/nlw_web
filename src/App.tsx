25afaf487215733f1939c4f4b37e231ceb3179a3
Marcio <blogdomarcio@live.com>
2021-10-20 19:10:59 -0300

    Add Message List, Form and LoginBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { useAuth } from './hooks/useAuth'
 
 
 export function App() {
+  const { user } = useAuth()
 
   return (
-    <>
-      <div className={styles.contextwraper}>
-        <h1> Blog do Marcio  </h1>
-        <LoginBox />
+    <div className="App">
+      <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
         <MessageList />
-      </div>
-
-    </>
+        {!!user ? <SendMessageForm /> : <LoginBox />}
+      </main>
+    </div>
   )
 }
-
